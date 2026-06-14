6cff5a2ece84d131e4b2e94d03faa4e03fdee3d1
Halil Kaya <2halilkaya734@gmail.com>
2026-06-14 13:47:57 +0300

    feat:First Commit

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+TODO: ReadMe en son düzgünce doldur.
---
+++ sudoku-maker/MainWindow.axaml.cs
@@ -0,0 +1,15 @@
+using Avalonia.Controls;
+
+namespace sudoku_maker;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_SizeChanged(object? sender, SizeChangedEventArgs e)
+    {
+    }
+}
